9f31e4eeab247c7d73f507bfa3ce4744b4c500bb
Philippa Hack <philippa.hack@gmail.com>
2016-05-07 11:55:26 +0100

    Extracted terrain inference code to its own file

--- undo-projection.py
+++ undo-projection.py
@@ -255,78 +255,10 @@ for e in newEdges:
 #   "Factor-out" the constant co-ordinate and pass the list of 2D coordinates to triangulate
 #   "Re-distribute" the constant co-ordinate
 
-# INFER HIDDEN TERRAIN
-def birdsEyeView((x,y,z)):
-  return (x,z)
+directedEdges = ## double count attached edges, add hidden paths w/o colour
+simplePolygons = segment.separateIntoPolygons(verts3D, directedEdges)
+for polygon in simplePolygons:
+  constantAxisCoordinate = ###
+  vertexTriples2D = triangulator.triangulate_v1(removeConstantAxis(polygon))
+  my_triangles = redistribute(constantAxisCoordinate, vertexTriples2D)
 
-def isXZaliased(v1,v2):
-  return birdsEyeView(v1) == birdsEyeView(v2)
-
-def groupXZAliased(path):
-  aliased = []
-  chord = [path[0]]
-  barrelShifted = path[1:] + [path[0]]
-  for v in barrelShifted:
-    if (isXZaliased(v, chord[-1])):
-      chord.append(v)
-    else:
-      aliased.append(chord)
-      chord = [v]
-  return aliased
-
-def zeroArea(triangle):
-  v1,v2,v3 = triangle
-  return v1 == v2 or v2 == v3 or v1 == v3
-
-print "Triangles to join the hidden white shards to the visible mesh"
-
-def floorHeight(contour):
-  return min(map(lambda (x,y,z) : y, contour))
-
-def ceilingHeight(contour):
-  return max(map(lambda (x,y,z) : y, contour))
-
-# A "wall" of rectangles joins the hidden floor/ceiling to the visible mesh
-# A rectangular wall segment is composed of two identical right triangles
-# j = (i+1) % len(aliasedVerts3D), but (+) can't go in a variable name
-def wallSegmentBounds(contour, y):
-  aliasedInXZPlane = groupXZAliased(contour)
-  flatContour = map(lambda vs : (vs[0][0], y, vs[0][2]), aliasedInXZPlane)
-  jaggedContour = map(lambda vs : (vs[0], vs[-1]), aliasedInXZPlane)
-  return zip(flatContour, jaggedContour)
-
-def connectToFloor(left_side, right_side):
-  flat_L, (jagged_first_L, jagged_last_L) = left_side
-  flat_R, (jagged_first_R, jagged_last_R) = right_side
-  triangle1 = (jagged_last_L, flat_R,         flat_L)
-  triangle2 = (jagged_last_L, jagged_first_R, flat_R)
-  return filter(lambda t : not zeroArea(t), [triangle1, triangle2])
-
-def connectToCeiling(left_side, right_side):
-  flat_L, (jagged_first_L, jagged_last_L) = left_side
-  flat_R, (jagged_first_R, jagged_last_R) = right_side
-  triangle1 = (flat_L, jagged_first_R, jagged_last_L)
-  triangle2 = (flat_L, flat_R,         jagged_first_R)
-  return filter(lambda t : not zeroArea(t), [triangle1, triangle2])
-
-def triangulateWall(contour, y, triangulate):
-  wallTriangles = []
-  a = wallSegmentBounds(contour, y)
-  cyclicPairs = [ (i,(i+1)%len(a)) for i in range(len(a)) ]
-  for (i,j) in cyclicPairs:
-    wallTriangles += triangulate(a[i], a[j])
-  return wallTriangles
-
-def addHiddenFloor(contour):
-  return triangulateWall(contour, floorHeight(contour), lambda t1, t2 : connectToFloor(t1,t2))
-
-def addHiddenCeiling(contour):
-  return triangulateWall(contour, ceilingHeight(contour), lambda t1, t2 : connectToCeiling(t1,t2))
-
-for contour in hiddenTerrainContours:
-  contourVerts3D = map(lambda v : uniqueVerts3D[v], contour)
-  tri = addHiddenFloor(contourVerts3D)
-  for t in tri:
-    print t 
-  # Triangulate floorContour (That becomes part of the hidden mesh)
-# Whether or not we add a floor or ceiling depends on which side is in the foreground
