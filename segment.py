a3789dc1455ad2a9abf5314c7ca090dc372c7ce6
Philippa Hack <philippa.hack@gmail.com>
2016-05-11 13:31:53 +0100

    Tests a face for completeness, and cycles through possible e2 until achieved

--- segment.py
+++ segment.py
@@ -56,6 +56,7 @@ def normalisedCrossProduct(delta1, delta2):
   # We need to distinguish cases where the surface normal renders the face invisble.
   # to avoid the possibiliy of treating the unattached contour as a face,
   # hence getting stuck because the normals change freely
+  # Though in the context of ignoring acyclic paths, this may not be necessary
   d1_x, d1_y, d1_z = delta1
   d2_x, d2_y, d2_z = delta2
   x = d1_y * d2_z - d1_z * d2_y
@@ -73,7 +74,6 @@ def separateIntoPolygons(verts3D, directedEdges):
     v_start, v_end = edge
     return (lambda nextEdge : nextEdge[0] == v_end)
 
-  # Change to reflect undirected edges
   edgesFollowing = {}
   for edge in directedEdges:
     edgesFollowing[edge] = filter(doesEdgeFollow(edge), directedEdges) 
@@ -91,13 +91,11 @@ def separateIntoPolygons(verts3D, directedEdges):
   def completeFace(e1,e2):
     face = [e1,e2]
     faceNormal = normalTo(e1,e2)
-    # Follow undirected edges. All because you might begin a face on two attached edges, and trace the wrong way
+ 
     def addNextCoplanarEdge(edge):
       foundNextEdge = False
       nextEdge = (0,0)
       for e in edgesFollowing[edge]:
-        # Direct undirected e according to edge's end vertex
-        # [(a,b), (b,c), (d,c)] -> [(a,b), (b,c), (c,d)]
         if (not foundNextEdge \
             and (e not in face) \
             and (not isReversedEdge(e,edge)) \
@@ -109,35 +107,26 @@ def separateIntoPolygons(verts3D, directedEdges):
         addNextCoplanarEdge(nextEdge)
 
     addNextCoplanarEdge(e2)
-    # (If successful...)
-    # Upon completion, direct edges clockwise
-    # Orientation test:
-    #   Draw a line +90 degrees from the center of a directed edge (center offset to avoid corner cases)
-    #   If it intersects an odd number of other edges, then clockwise
-    #   Else, reverse all edges
-    # What if unsuccessful? return [], and test in clipFace?
-    # Why do we need to follow undirected edges? The idea itself is a bit of a fudge, because it is using directed edges not explictly beloning to that face
-    return face
+    isCompleteFace = face[-1][1] == face[0][0]
+    return face if (isCompleteFace) else []
+  
+  def normalIsUnitVector(e1,e2):
+    n = normalTo(e1,e2)
+    return (n==(1,0,0) or n==(0,1,0) or n==(0,0,1))
 
   polygons = []
   def clipFace(es):
     if (len(es) >= 3):
-      # Choose directed edge 1
       e1 = es[0]
-      # Choose directed edge 2 by filtering es such that:
-      #   e2 follows e1
-      #   e1xe2 goes in a unit vector direction [subsumed not isReversedEdge test]
-
-      e2 = filter(lambda e2 : not isReversedEdge(e1,e2), edgesFollowing[e1])[0] # Is Python's filter lazy!?
-      polygon = completeFace(e1, e2)
-      # e1,e2 still may not represent a face.
-      # May fail safe, if in which case the path terminates without rejoining.
-      # Select the next possible e1, repeat until face found.
+      e2s = filter(lambda e2 : doesEdgeFollow(e1)(e2) and normalIsUnitVector(e1,e2), es[1:])
+      polygon = []
+      while (polygon == []):
+        polygon = completeFace(e1, e2s.pop())
+        # Can e2s exhaust without completing a face? Shouldn't be possible, as each undirected edge represents a face to be completed
       polygons.append(polygon)
-      # Remove polygon from directedEdges and call recursively
       clipFace(filter(lambda edge : edge not in polygon, es))
 
   clipFace(directedEdges)
   return polygons
 
-#print separateIntoPolygons(sampleVerts3D, sampleDirectedEdges)
+print separateIntoPolygons(sampleVerts3D, sampleDirectedEdges)
