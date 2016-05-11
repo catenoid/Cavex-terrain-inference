02edc5e3aefd6e2c7e2e53fb3b46540cea0e346d
Philippa Hack <philippa.hack@gmail.com>
2016-05-11 12:57:33 +0100

    Requires reworking. Too many comments, time to start changing things

--- segment.py
+++ segment.py
@@ -53,6 +53,9 @@ def unit(n):
   return n/n if (n != 0) else 0
 
 def normalisedCrossProduct(delta1, delta2):
+  # We need to distinguish cases where the surface normal renders the face invisble.
+  # to avoid the possibiliy of treating the unattached contour as a face,
+  # hence getting stuck because the normals change freely
   d1_x, d1_y, d1_z = delta1
   d2_x, d2_y, d2_z = delta2
   x = d1_y * d2_z - d1_z * d2_y
@@ -70,6 +73,7 @@ def separateIntoPolygons(verts3D, directedEdges):
     v_start, v_end = edge
     return (lambda nextEdge : nextEdge[0] == v_end)
 
+  # Change to reflect undirected edges
   edgesFollowing = {}
   for edge in directedEdges:
     edgesFollowing[edge] = filter(doesEdgeFollow(edge), directedEdges) 
@@ -81,15 +85,23 @@ def separateIntoPolygons(verts3D, directedEdges):
     d2 = subtract3(verts3D[v3], verts3D[v2]) 
     return normalisedCrossProduct(d2,d1)
 
+  def areColinear(e1,e2):
+    return (normalTo(e1,e2) == (0,0,0))
+
   def completeFace(e1,e2):
     face = [e1,e2]
     faceNormal = normalTo(e1,e2)
-
+    # Follow undirected edges. All because you might begin a face on two attached edges, and trace the wrong way
     def addNextCoplanarEdge(edge):
       foundNextEdge = False
       nextEdge = (0,0)
       for e in edgesFollowing[edge]:
-        if (not foundNextEdge and ((normalTo(edge,e) == faceNormal) or (normalTo(edge,e) == (0,0,0))) and (e not in face) and (not isReversedEdge(e,edge))):
+        # Direct undirected e according to edge's end vertex
+        # [(a,b), (b,c), (d,c)] -> [(a,b), (b,c), (c,d)]
+        if (not foundNextEdge \
+            and (e not in face) \
+            and (not isReversedEdge(e,edge)) \
+            and ((normalTo(edge,e) == faceNormal) or areColinear(edge,e))):
           foundNextEdge = True
           nextEdge = e
       if (foundNextEdge):
@@ -97,27 +109,32 @@ def separateIntoPolygons(verts3D, directedEdges):
         addNextCoplanarEdge(nextEdge)
 
     addNextCoplanarEdge(e2)
+    # (If successful...)
+    # Upon completion, direct edges clockwise
+    # Orientation test:
+    #   Draw a line +90 degrees from the center of a directed edge (center offset to avoid corner cases)
+    #   If it intersects an odd number of other edges, then clockwise
+    #   Else, reverse all edges
+    # What if unsuccessful? return [], and test in clipFace?
+    # Why do we need to follow undirected edges? The idea itself is a bit of a fudge, because it is using directed edges not explictly beloning to that face
     return face
 
   polygons = []
   def clipFace(es):
     if (len(es) >= 3):
+      # Choose directed edge 1
       e1 = es[0]
-      print "clipface first edge is", e1
-      e2 = filter(lambda e2 : not isReversedEdge(e1,e2), edgesFollowing[e1])[0] # is Python's filter lazy?
-      # Big problem:
-      # If tracing attached edges, e2 might not be clockwise from e1. You can't know until you've completed the shape which way it is oriented
-      # Yet it's important to remove directed edges, as both undirected edges and vertices are reused across the mesh
-      # How about: When tracing a face, follow undirected edges. Upon completion, orient clockwise, then remove and call recursively
-      # How can you tell if a shape is clockwise?
-      #   Draw a line +90 degrees to an edge. If it intersects the other sides of the polygon an odd number of times, then it's CW
-      # "Edge"-cases (har har) for when the intersection is at one of the vertices, and when the lines are colinear (not just parallel) 
-      # so if we are now traversing undirected edges, how do we not move backwards? edgesFollowing will have to be modified...
+      # Choose directed edge 2 by filtering es such that:
+      #   e2 follows e1
+      #   e1xe2 goes in a unit vector direction [subsumed not isReversedEdge test]
 
-      # Can this list be empty? -- I'm returning here to say, yes, it just indexed out of range
+      e2 = filter(lambda e2 : not isReversedEdge(e1,e2), edgesFollowing[e1])[0] # Is Python's filter lazy!?
       polygon = completeFace(e1, e2)
-      print "Polygon found:", polygon
+      # e1,e2 still may not represent a face.
+      # May fail safe, if in which case the path terminates without rejoining.
+      # Select the next possible e1, repeat until face found.
       polygons.append(polygon)
+      # Remove polygon from directedEdges and call recursively
       clipFace(filter(lambda edge : edge not in polygon, es))
 
   clipFace(directedEdges)
